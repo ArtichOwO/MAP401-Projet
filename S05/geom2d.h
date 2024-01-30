90ef5308786f19a61002a5e1e2f7a25035847d9a
ArtichautCosmique <lelolartichaut@laposte.net>
2024-01-30 13:36:15 +0100

    tache 3 fichiers de base

---
+++ S05/geom2d.h
@@ -0,0 +1,33 @@
+#ifndef GEOM2D_H
+#define GEOM2D_H
+
+typedef struct Vecteur_
+{
+    double x, y; /* Coordonnées */
+} Vecteur;
+
+typedef struct Point_
+{
+    double x, y; /* Coordonnées */
+} Point;
+
+Point set_point(double x, double y);
+Vecteur set_vect(double x, double y);
+
+Point add_point(Point P1, Point P2);
+Vecteur add_vect(Vecteur A, Vecteur B);
+
+Point produit_reel_point(Point P, double l);
+Vecteur produit_reel_vecteur(Vecteur V, double l);
+
+Vecteur vect_bipoint(Point A, Point B);
+
+double produit_scalaire(Vecteur A, Vecteur B);
+
+double point_norme(Point P1, Point P2);
+double vect_norme(Vecteur V);
+
+void afficher_point(Point P);
+void afficher_vecteur(Vecteur V);
+
+#endif
