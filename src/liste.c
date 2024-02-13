6d14f9eb2f7c61d5167a3b6e53fb17eb01a0c191
milochimilo <mmgp031@gmail.com>
2024-02-13 14:53:43 +0100

    correc 1

--- src/liste.c
+++ src/liste.c
@@ -56,24 +56,24 @@ void supprimer_premier_element(Cellule *L) {
     }
 }
 
-// Tableau_Points sequence_points_tableau(Cellule *L) {
-//     int Taille = longueur_liste(L);
+Tableau_Points sequence_points_tableau(Cellule *L) {
+    int Taille = longueur_liste(L);
 
-//     if (Taille != 0) {
-//         Tableau_Points tab = 
-//         Cellule *current = L;
+    if (Taille != 0) {
+        Tableau_Points tab = 
+        Cellule *current = L;
 
-//         for (int i = 0; i < Taille; i++) {
-//             Tab[i] = current->p;
-//             current = current->n;
-//         }
-//     }
-// }
+        for (int i = 0; i < Taille; i++) {
+            Tab[i] = current->p;
+            current = current->n;
+        }
+    }
+}
 
 void imprimer_liste(Cellule *L) {
     Cellule *current = L;
 
-    while (L) {
+    while (current) {
         afficher_point(current->p);
         current = current->n;
     }
