350d78b1a0739e97956a221eda16cf678161a5ce
ArtichautCosmique <lelolartichaut@laposte.net>
2024-02-13 14:24:39 +0100

    test_contour .contours

--- src/test_contour.c
+++ src/test_contour.c
@@ -3,6 +3,7 @@
 #include "liste.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char * argv[]) {
 	if (argc < 2) {
@@ -14,6 +15,18 @@ int main(int argc, char * argv[]) {
 	Cellule * L = trouver_contour(I);
 	imprimer_liste(L);
 	printf("Nombre de segments : %i\n", longueur_liste(L) - 1);
+
+	char * ext = ".contours";
+	*strrchr(argv[1], '.') = 0;
+	argv[1] = realloc(argv[1], strlen(argv[1])+strlen(ext));
+	char * filename = strcat(argv[1], ext);
+	FILE * fd = fopen(filename, "w");
+
+	fprintf(fd, "1\n\n%i\n", longueur_liste(L));
+	while (L) {
+		fprintf(fd, " %.1lf %.1lf", L->p.x, L->p.y);
+		L = L->n;
+	}
 
 	return 0;
 }
