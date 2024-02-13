2d011a08f3c224829d502e87934662416c0fb183
milochimilo <mmgp031@gmail.com>
2024-02-13 14:54:56 +0100

    corrc 0

--- src/liste.c
+++ src/liste.c
@@ -56,19 +56,19 @@ void supprimer_premier_element(Cellule *L) {
     }
 }
 
-Tableau_Points sequence_points_tableau(Cellule *L) {
-    int Taille = longueur_liste(L);
+// Tableau_Points sequence_points_tableau(Cellule *L) {
+//     int Taille = longueur_liste(L);
 
-    if (Taille != 0) {
-        Tableau_Points tab = 
-        Cellule *current = L;
+//     if (Taille != 0) {
+//         Tableau_Points tab = 
+//         Cellule *current = L;
 
-        for (int i = 0; i < Taille; i++) {
-            Tab[i] = current->p;
-            current = current->n;
-        }
-    }
-}
+//         for (int i = 0; i < Taille; i++) {
+//             Tab[i] = current->p;
+//             current = current->n;
+//         }
+//     }
+// }
 
 void imprimer_liste(Cellule *L) {
     Cellule *current = L;
