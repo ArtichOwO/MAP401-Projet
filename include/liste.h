a26e1a306044e7143d3199dfd379da180dcdc99c
ArtichautCosmique <lelolartichaut@laposte.net>
2024-02-13 15:23:59 +0100

    suppr fonctions

--- include/liste.h
+++ include/liste.h
@@ -27,18 +27,6 @@ int longueur_liste(Liste L);
 /* Ajoute un element dans la liste */
 void ajouter_element_liste(Liste * L, Point e);
 
-/* supprime la liste */
-void supprimer_liste(Liste * L);
-
-/* concatene L2 a L1 */
-void concatener_liste(Liste * L1, Liste * L2);
-
-/* supprime le premier element de la liste */
-void supprimer_premier_element(Liste * L);
-
-// /* convertit la liste en tableau */
-// Tableau_Points sequence_points_tableau(Liste L);
-
 /* Affiche le contours de l'image */
 void imprimer_liste(Liste L);
 
--- src/liste.c
+++ src/liste.c
@@ -26,50 +26,6 @@ void ajouter_element_liste(Liste * L, Point e) {
     }
 }
 
-void supprimer_liste(Liste *L) {
-    Cellule *c = malloc(sizeof(Cellule));
-
-    while (c) {
-        
-    }
-}
-
-void concatener_liste(Liste *L1, Liste *L2) {
-    Cellule *current = L1->t;
-
-    if (current == NULL)
-    {
-        L1->t = L2->t;
-    }
-
-    while (current->n) {
-        current = current->n;
-    }
-    current->n = L2->t;
-}
-
-void supprimer_premier_element(Liste *L) {
-    if (L->t) {
-        Cellule *c = L->t;
-        L->t = L->t->n;
-        free(c);
-    }
-}
-
-// Tableau_Points sequence_points_tableau(Cellule *L) {
-//     int Taille = longueur_liste(L);
-
-//     if (Taille != 0) {
-//         Tableau_Points tab = 
-//         Cellule *current = L;
-
-//         for (int i = 0; i < Taille; i++) {
-//             Tab[i] = current->p;
-//             current = current->n;
-//         }
-//     }
-// }
-
 void imprimer_liste(Liste L) {
     Cellule *current = L.t;
 
